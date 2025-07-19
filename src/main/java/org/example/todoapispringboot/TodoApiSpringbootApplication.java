884beab950ce04c7802afd0c44aa830d538b920a
123Brijesh44aa <oddsworld0121@gmail.com>
2025-07-19 17:46:32 +0530

    creating a basic todo rest api in spring boot

---
+++ src/main/java/org/example/todoapispringboot/TodoApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.todoapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/todoapispringboot/TodoController.java
@@ -0,0 +1,21 @@
+package org.example.todoapispringboot;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+    private static List<Todo> todos;
+
+    public TodoController(){
+        todos = new ArrayList<>();
+        todos.add(new Todo(1,true, "Todo 2",1));
+        todos.add(new Todo(2,false,"Todo 2", 2));
+    }
+
+
+
+}
